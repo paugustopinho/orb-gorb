6d319bdfad0638fee25c51b0be123292da0aa73d
paugustopinho <pedro.pinho316@gmail.com>
2023-04-26 08:04:45 -0500

    4

--- Assets/Dialouge/Dialouge.cs
+++ Assets/Dialouge/Dialouge.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.UI;
 using TMPro;
+using System;
 
 public class Dialouge : MonoBehaviour
 {
     [SerializeField] List<string> lines, response, answer;
+    [SerializeField] GameObject button1, button2;
     [SerializeField] TextMeshProUGUI output, answer1, answer2;
-    float textSpeed = 0.25f;
+    float textSpeed = 0.1f;
     bool listenforAnswer;
     int answerTo1, answerTo2;
 
@@ -23,12 +25,16 @@ public class Dialouge : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 listenforAnswer = false;
-                StartCoroutine(TypeLine(answerTo1));
+                button1.SetActive(false);
+                button2.SetActive(false);
+                StartCoroutine(TypeLine(answerTo1, answerTo1 == 0));
             }
             else if (Input.GetKeyDown(KeyCode.E))
             {
                 listenforAnswer = false;
-                StartCoroutine(TypeLine(answerTo2));
+                button1.SetActive(false);
+                button2.SetActive(false);
+                StartCoroutine(TypeLine(answerTo2, answerTo2 == 0));
             }
         }
     }
@@ -58,15 +64,25 @@ public class Dialouge : MonoBehaviour
 
     void choice(int choice1, int choice2)
     {
+        button1.SetActive(true);
+        button2.SetActive(true);
         answer1.text = response[choice1];
         answer2.text = response[choice2];
-        listenforAnswer= true;
+        listenforAnswer = true;
         answerTo1 = choice1;
         answerTo2 = choice2;
     }
-    IEnumerator TypeLine(int index)
+
+    IEnumerator TypeLine(int index, bool doesContinue)
     {
-        //finish
-        yield return new WaitForSeconds(1f);
+        output.text = "";
+        foreach (char c in answer[index].ToCharArray())
+        {
+            output.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+        yield return new WaitForSeconds(textSpeed * 2f);
+        if (doesContinue)
+            choice(2, 3);
     }
 }
