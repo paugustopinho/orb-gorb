dabbdf9a23ab3fab13b648341832ac9e19612dfe
paugustopinho <pedro.pinho316@gmail.com>
2023-05-04 08:04:38 -0500

    8

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,8 +11,9 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     float walkSpeed, sprintSpeed;
     float horizontal, vertical;
+    [SerializeField] float coyoteTimeCounter, coyoteTime;
     [SerializeField] float jumpForce;
-    [SerializeField] bool isGrounded;
+    [SerializeField] bool isGrounded, secondJump;
     [SerializeField] LayerMask ground;
 
     void Start()
@@ -44,21 +45,40 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         transform.rotation = Quaternion.Euler(0f, cam.eulerAngles.y, 0f);
+        //if (!isGrounded) { direction = new Vector3(0f, 0f, vertical).normalized; }
 
         if (direction.magnitude >= 0.1f)
         {
             Vector3 moveDir = Quaternion.Euler(0f, cam.eulerAngles.y, 0f) * direction;
             rb.velocity = new Vector3(moveDir.x * speed, rb.velocity.y, moveDir.z * speed);
         }
+        else if(direction.magnitude == 0f)
+        {
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+        }
     }
 
     void Jump()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.25f, ground);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.5f, ground);
+        if (isGrounded) { 
+            secondJump = true;
+            coyoteTimeCounter = coyoteTime;
+        }
+        else { coyoteTimeCounter -= Time.deltaTime; }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (rb.velocity.y > 0f) { coyoteTimeCounter = 0f; }
+
+        if (Input.GetButtonDown("Jump") && coyoteTimeCounter > 0f)
         {
+            coyoteTimeCounter = 0f;
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
+        /*else if ( Input.GetButtonDown("Jump") && secondJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            secondJump = false;
+        }*/
     }
 }
