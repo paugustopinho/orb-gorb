0eeb147ff2aa6d89f7380ea1d3074daea8d09e1e
paugustopinho <pedro.pinho316@gmail.com>
2023-05-08 08:02:51 -0500

    10

---
+++ Assets/LavaPlatform.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LavaPlatform : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 startpos;
+    public bool swaying;
+
+    private void Start()
+    {
+        startpos = transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rb.velocity = new Vector3(0f, -.05f, 0f);
+            swaying = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rb.velocity = new Vector3(0f, .1f, 0f);
+        }
+    }
+
+    private void Update()
+    {
+        if (transform.position.y >= startpos.y) { rb.velocity = Vector3.zero; swaying = true; }
+        if (swaying)
+        {
+            float y = Mathf.PingPong(Time.time * .5f, .5f);
+            transform.position = new Vector3(transform.position.x, startpos.y - y, transform.position.z);
+        }
+    }
+}
